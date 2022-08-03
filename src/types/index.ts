c04f56e75ffbb9eca7a389dcd3dff37bf0c5f5d0
unknown <egkirpichev@gmail.com>
2022-08-04 00:22:21 +0300

    feat(budget-app): add types file with Color enum

---
+++ src/types/index.ts
@@ -0,0 +1,10 @@
+export enum Color {
+	Black = "#000000",
+	White = "#FFFFFF",
+	LightBlue = "#7CC6FE",
+	LightPurple = "#CCD5FF",
+	LightPink = "#E7BBE3",
+	Red = "#FF0000",
+	LightGrey = "#999999",
+	Blue = "#23C9FF"
+}
