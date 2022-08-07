baec31ccf3a996eeac0b4551495ceea04378932c
unknown <egkirpichev@gmail.com>
2022-08-07 16:30:39 +0300

    refactor(budget-app): move currency select styles to a separate file

--- src/components/Select/CustomSelect.tsx
+++ src/components/Select/CustomSelect.tsx
@@ -2,6 +2,7 @@ import Select, { StylesConfig } from "react-select"
 import { Currency } from "../../config/currency"
 import { ICurrencyOption } from "../../types"
 import { Color } from "../../ui/color"
+import { customStyles } from "./styles"
 
 export const CustomSelect = () => {
 	const currencyOptions: ICurrencyOption[] = [
@@ -10,36 +11,6 @@ export const CustomSelect = () => {
 		{value: Currency.GBR, label: "GBR"},
 	]
 	
-	const customStyles: StylesConfig<ICurrencyOption, boolean> = {
-		control: (styles) => ({
-			...styles,
-			minHeight: `30px`,
-			border: `1px solid ${Color.LightGrey}`,
-		}),
-
-		valueContainer: (styles) => ({
-			...styles,
-			padding: 0,
-			textAlign: `center`,
-			fontWeight: `400`,
-			fontSize: `12px`,
-			lineHeight: `15px`,
-		}),
-
-		indicatorsContainer: (styles) => ({
-			...styles,
-			div: {
-				padding: `0px 5px`,
-			}
-		}),
-
-		indicatorSeparator: (styles) => ({
-			...styles,
-			border: `1px solid ${Color.LightGrey}`,
-		})
-	}
-
-
 	return (
 		<Select options={currencyOptions} isMulti={false} defaultValue={currencyOptions[0]} styles={customStyles}/>
 	)
---
+++ src/components/Select/styles.ts
@@ -0,0 +1,32 @@
+import { StylesConfig } from "react-select";
+import { ICurrencyOption } from "../../types";
+import { Color } from "../../ui/color";
+
+export const customStyles: StylesConfig<ICurrencyOption, boolean> = {
+	control: (styles) => ({
+		...styles,
+		minHeight: `30px`,
+		border: `1px solid ${Color.LightGrey}`,
+	}),
+
+	valueContainer: (styles) => ({
+		...styles,
+		padding: 0,
+		textAlign: `center`,
+		fontWeight: `400`,
+		fontSize: `12px`,
+		lineHeight: `15px`,
+	}),
+
+	indicatorsContainer: (styles) => ({
+		...styles,
+		div: {
+			padding: `0px 5px`,
+		}
+	}),
+
+	indicatorSeparator: (styles) => ({
+		...styles,
+		border: `1px solid ${Color.LightGrey}`,
+	})
+}
