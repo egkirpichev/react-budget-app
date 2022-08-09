159676f22b3df61a5b912c8bf175300869aa1319
unknown <egkirpichev@gmail.com>
2022-08-10 01:06:48 +0300

    feat(budget-app): link contexts to Remaining components, implement component style changes when overspent

--- src/components/Remaining/Remaining.tsx
+++ src/components/Remaining/Remaining.tsx
@@ -1,12 +1,34 @@
+import { useEffect, useState } from "react";
+import { useBudgetContext } from "../../context/BudgetContext/BudgetContext";
 import { useCurrencyContext } from "../../context/CurrencyContext/CurrencyContext";
+import { useExpensesContext } from "../../context/ExpencesContext/ExpencesContext";
+import { IExpense } from "../../types";
 import { StyledRemaining, Title } from "./styles";
 
 export const Remaining = () => {
   const { currency } = useCurrencyContext();
+  const { expenses } = useExpensesContext();
+  const { budget } = useBudgetContext();
+
+  const spent: number = [...expenses].reduce(
+    (spent: number, expense: IExpense): number => {
+      return expense ? (spent += Number(expense.cost)) : spent;
+    },
+    0
+  );
+  const remaining: number = budget - spent;
+
+  const [isOverspnding, setIsOverspending] = useState<boolean>(false);
+
+  useEffect(() => {
+		if(remaining < 0) {
+			setIsOverspending(true)
+		}
+	}, [spent]);
 
   return (
-    <StyledRemaining>
-      <Title>{`Remaining: ${currency.value}2000`}</Title>
+    <StyledRemaining isOverspnding={isOverspnding}>
+      <Title isOverspnding={isOverspnding}>{`Remaining: ${currency.value}${remaining}`}</Title>
     </StyledRemaining>
   );
 };
--- src/components/Remaining/styles.ts
+++ src/components/Remaining/styles.ts
@@ -1,18 +1,23 @@
 import styled from "styled-components";
 import { Color } from "../../ui/color";
 
-export const StyledRemaining = styled.div`
+interface IProps {
+	isOverspnding: boolean
+}
+
+export const StyledRemaining = styled.div<IProps>`
 	grid-column: 1/3;
 	padding: 32px 20px;
 	display: flex;
 	align-items: center;
 	justify-content: space-between;
-	background: ${Color.LightPurple};
+	background: ${(props => props.isOverspnding ? Color.Red : Color.LightPurple)};
 	border-radius: 10px;
 `
 
-export const Title = styled.span`
-	font-weight: 500;
+export const Title = styled.span<IProps>`
+	font-weight: ${(props => props.isOverspnding ? 700 : 500)};
 	font-size: 20px;
 	line-height: 24px;
+	color: ${(props => props.isOverspnding ? Color.White : Color.Black)};
 `
