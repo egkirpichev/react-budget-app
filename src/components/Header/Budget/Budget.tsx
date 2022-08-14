9dfd0f71284814ae66259378b08f0d75c0450b7f
unknown <egkirpichev@gmail.com>
2022-08-14 14:53:45 +0300

    feat(react-budget-app): set autofocus on budget input upon every edit

--- src/components/Header/Budget/Budget.tsx
+++ src/components/Header/Budget/Budget.tsx
@@ -26,6 +26,7 @@ export const Budget = () => {
           placeholder="Enter  budget ..."
           type="text"
           pattern="[0-9]+"
+          autoFocus
           {...budgetInput}
         />
       ) : (
